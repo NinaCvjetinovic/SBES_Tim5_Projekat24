cdfa050506053ebdd5069c419f1dbaeb01ecdeea
NinaCvjetinovic <ncvjetinovic@gmail.com>
2023-01-09 12:57:08 +0100

    Kreiranje XML fajla i preuzimanje vrijednosti iz njega

---
+++ Projekat_SBES/Manager/InitializeUsersFromXml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Manager
+{
+    public class InitializeUsersFromXml
+    {
+        private void InitializeUsersFromXml1(string filePath)
+        {
+            XDocument doc = XDocument.Load("acl.xml");
+
+            var users = doc.Root.Elements("user");
+            foreach (var userElement in users)
+            {
+                string username = userElement.Attribute("name").Value;
+                //string organization = userElement.Attribute("organization").Value;
+                var privileges = userElement.Elements("permission").Select(p => p.Value);
+
+            }
+        }
+
+   
+    }
+}
+
